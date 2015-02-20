2a6b8f33a8871560ab5c9b90f63ca9181454e32b
HassanAMian <hassanmian83@gmail.com>
2015-02-20 00:18:16 -0500

    Edit Spec Refactor

--- spec/features/todo_lists/edit_spec.rb
+++ spec/features/todo_lists/edit_spec.rb
@@ -1,17 +1,25 @@
 require 'spec_helper'
 
 describe "Editing todo lists" do
+	let!(:todo_list) { todo_list = TodoList.create(title: "Foo", description: "Bar") }
+	def update_todo_list(options={})
+		options[:title] ||= "Foo"
+		options [:description] ||= "Bar"
 
-	it "updates a todo list successfully" do
-		todo_list = TodoList.create(title: "Foo", description: "Bar")
+		todo_list = options[:todo_list]
+		
 		visit "/todo_lists"
 		within "#todo_list_#{todo_list.id}" do
 			click_link "Edit"
 		end 
 
-		fill_in "Title", with: "New"
-		fill_in "Description", with: "New"
+		fill_in "Title", with: options[:title]
+		fill_in "Description", with: options[:description]
 		click_button "Update Todo list"
+	end
+	
+	it "updates a todo list successfully" do
+		update_todo_list todo_list: todo_list, title: "New", description: "New"
 
 		todo_list.reload
 
